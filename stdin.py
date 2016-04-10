859c386e852eca02efd38cb6d0e39aa947c3b0b0
emrah3009 <emr3234@hotmail.com>
2016-04-09 20:28:23 -0400

    Update stdin.py

--- stdin.py
+++ stdin.py
@@ -2,7 +2,7 @@
 # This code is a tool to read from stdin and writes to a file
 import sys
 
-with open('report.txt','w') as f1: # appends the line to the text file
+with open('report.txt','w') as f1: # writes the line to the text file
     while 1:
         try:
             line = sys.stdin.read(1)
