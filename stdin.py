e6173874a76513a17df1634ca1284ffb7d644406
emrah3009 <emr3234@hotmail.com>
2016-04-11 00:27:20 -0400

    Update stdin.py
    In order to run program fast, I changed read(1) and instead of it I put os.read(fd,1024). By this way, the program will read 1024 bytes from fd file. At the end of the file, empty string will return additionally length of the line will be zero.

--- stdin.py
+++ stdin.py
@@ -1,14 +1,19 @@
 # By Emrah KORKMAZ
 # This code is a tool to read from stdin and writes to a file
-import sys
+
+import sys, os
 
 with open('report.txt','w') as f1: # writes the line to the text file
     while 1:
         try:
-            line = sys.stdin.read(1)
+            fd = sys.stdin.fileno()
+            line = os.read(fd,1024*1) #reads lines
             if len(line) == 0:
                 break
             f1.write(line)
             f1.flush()
-        except KeyboardInterrupt:       # stops with keybord interrupt
+        except (KeyboardInterrupt):       # stops with keybord interrupt
             break
+
+
+
