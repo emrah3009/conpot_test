dfd5807ddd960fce34880549e6431a3b3ea66e78
emrah3009 <emr3234@hotmail.com>
2016-04-11 00:18:07 -0400

    Update unittest1.py
    "sys.stdin" can be run in the function. I also added an empty file to test in the unittest.

--- unittest1.py
+++ unittest1.py
@@ -7,7 +7,9 @@ def testing(handle):
     with open('report.txt','w') as f1: # appends the line to the text file
         while 1:
             try:
-                line = handle.read(1) #reads lines
+                fd = handle.fileno()
+                line = os.read(fd,1024)
+                #line = handle.read(1) #reads lines
                 if len(line) == 0:
                     break
                 f1.write(line)
@@ -16,19 +18,28 @@ def testing(handle):
                 break
 
 
-with open('moby.txt','r') as f1:
-    testing(f1)
+def reading(files): # reads files and test in the stdin code
+    with open(files,'r') as f1:
+        testing(f1)
 
 
 import unittest
 
 class Mytest(unittest.TestCase):
-    def test_predicted(value):
+    def test_predicted1(value): # unit testing with a file
+        reading('moby.txt')
         value.assertEqual(os.path.getsize('report.txt'), os.path.getsize('moby.txt'))
+ 
+    def test_predicted2(value): # unit testing with an empty file
+        reading('emptyfile.txt')
+        value.assertEqual(os.path.getsize('report.txt'), os.path.getsize('emptyfile.txt'))        
 
+    def test_predicted3(value): # unit testing with stdin
+         testing(sys.stdin)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
 
 
-##testing(sys.stdin)
